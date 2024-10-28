f4bc9e36872a39117d10df8048569411e2157f0a
Ode Malfait <ode.malfait@student.ucll.be>
2024-10-28 13:47:57 +0100

    added code for swagger and added userRouter

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,9 +1,10 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import { userRouter } from './controller/user.routes';
 
 const app = express();
 dotenv.config();
@@ -11,10 +12,29 @@ const port = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+//routes
+app.use('/users', userRouter);
 
 app.get('/status', (req, res) => {
     res.json({ message: 'Back-end is running...' });
 });
+
+//swagger
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Courses API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./controller/*.routes.ts'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//generic error message:
+//hier nog zetten!!!
 
 app.listen(port || 3000, () => {
     console.log(`Back-end is running on port ${port}.`);
