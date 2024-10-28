e275acfca05fda9f5c5e3b9b28664df0524b2ed6
Ode Malfait <ode.malfait@student.ucll.be>
2024-10-28 14:18:09 +0100

    added origin to app.ts for front-end

--- back-end/app.ts
+++ back-end/app.ts
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin:'http://localhost:8080'}));
 app.use(bodyParser.json());
 //routes
 app.use('/users', userRouter);
