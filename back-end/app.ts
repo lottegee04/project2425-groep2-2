f9aa281412bbb0ce0e20eab53d246dbe542eb256
Ode Malfait <ode.malfait@student.ucll.be>
2024-11-02 16:32:32 +0100

    added generic error message in app.ts

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import express, { NextFunction } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
@@ -36,7 +36,15 @@ const swaggerSpec = swaggerJSDoc(swaggerOpts);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //generic error message:
-//hier nog zetten!!!
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ status: 'unauthorized', message: err.message });
+    } else if (err.name === 'CoursesError') {
+        res.status(400).json({ status: 'domain error', message: err.message });
+    } else {
+        res.status(400).json({ status: 'application error', message: err.message });
+    }
+});
 
 app.listen(port || 3000, () => {
     console.log(`Back-end is running on port ${port}.`);
