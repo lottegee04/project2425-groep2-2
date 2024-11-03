0d4631d0fe68ad6d28f5fc47dc1035fd813f0945
lottegee04 <lotte.geeraerts@student.ucll.be>
2024-11-03 14:06:46 +0100

    fixed imports

--- front-end/components/tasks/TaskOverview.tsx
+++ front-end/components/tasks/TaskOverview.tsx
@@ -1,4 +1,4 @@
-import { Task } from "@/types"
+import { Task } from "../../types"
 import { table } from "console"
 import React from "react"
 
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
-import Header from '@/components/header';
+import Header from '../components/header';
 
 const Home: React.FC = () => {
   return (
