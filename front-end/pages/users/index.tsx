4aa938ca5b3a8d3e75797a4fca98876b7d256bad
Ode Malfait <ode.malfait@student.ucll.be>
2024-11-03 19:07:18 +0100

    tasks from users now appear when you click

--- front-end/pages/users/index.tsx
+++ front-end/pages/users/index.tsx
@@ -8,11 +8,13 @@ import Header from '../../components/header';
 import TaskOverview from '../../components/tasks/TaskOverview';
 import UserService from '../../services/UserService';
 import UserOverview from '../../components/users/UserOverview';
+import TasksByUser from '../../components/tasks/TasksByUser';
 
 
 
 const Users: React.FC = () => {
     const [users,setUsers] = useState<Array<User>>();
+    const [selectedUser,setSelectedUser] = useState<User>();
 
 
     const getUsers = async () =>{
@@ -32,10 +34,18 @@ const Users: React.FC = () => {
     </Head>
     <Header/>
     <main className='d-flex  flex-column '>
-    <h1 className='align-self-center'>Users</h1>
+    <h1 className='align-self-center'>Users Overview</h1>
+    <p className='align-self-center'>Click on the user to see their tasks.</p>
     <section className='align-self-center d-flex flex-row p-2 '>
-    <UserOverview users={users}></UserOverview>
+    <UserOverview users={users} selectUser={setSelectedUser}></UserOverview>
     </section>
+    {selectedUser && (
+         <section className='align-self-center d-flex flex-column p-2 '>
+         <h2> Tasks from "{selectedUser.username}"</h2>
+         <TasksByUser user={selectedUser}></TasksByUser>
+       </section>
+    )}
+   
     </main>
     </>
   );
