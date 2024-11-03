3b484b39ebb70be5291f2db4ac4be85af8dc2eb6
Ode Malfait <ode.malfait@student.ucll.be>
2024-11-03 18:44:28 +0100

    made tasksByUser component

---
+++ front-end/components/tasks/TasksByUser.tsx
@@ -0,0 +1,33 @@
+import { Task, User } from "../../types"
+import { table } from "console"
+import React from "react"
+
+
+type Props = {
+    user:User
+}
+
+const TasksByUser: React.FC<Props> = ({user}) =>{
+    return (
+        <>
+        {user && 
+        user.tasks.map((task, index)=>(
+            <table className="border border-success border-opacity-50 border-4 m-2 bg-secondary bg-opacity-25">
+            <tr key={index}>
+                <td>Description: </td>
+                <td>{task.description}</td>
+            </tr>
+            <tr>
+                <td>Sidenote:</td>
+                <td>{task.sidenote}</td>
+            </tr>
+            </table>
+        ))
+        }
+        {!user.tasks&&
+        <p>No Active Tasks for this user</p>}
+        </>
+    )
+}
+
+export default TasksByUser;
