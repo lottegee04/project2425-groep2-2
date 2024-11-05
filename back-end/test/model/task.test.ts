8ecbca433c216d7811f431b270515509740aafe3
OdeMalfaitUCLL <ode.malfait@student.ucll.be>
2024-11-05 13:29:10 +0100

    changed task.status to task.done

--- back-end/test/model/task.test.ts
+++ back-end/test/model/task.test.ts
@@ -1,20 +1,30 @@
-import {addDays} from "date-fns";
-import { Task } from "../../model/task";
-import { start } from "repl";
-import { Priority } from "../../model/priority";
+import { addDays } from 'date-fns';
+import { Task } from '../../model/task';
+import { start } from 'repl';
+import { Priority } from '../../model/priority';
 
 //given:
 const id = 1;
-const description = "Boodschappen";
-const sidenote ="Doe boodschappen";
+const description = 'Boodschappen';
+const sidenote = 'Doe boodschappen';
 const startDate = new Date();
 //deadline morgen te laten zijn
-const deadline = addDays(startDate,1);
-const priority = new Priority({levelName:"basic",colour:"green"})
+const deadline = addDays(startDate, 1);
+const priority = new Priority({ levelName: 'basic', colour: 'green' });
 
-test("given: valid task parameters; when: creating a task;then: a task with the correct parameters is created;", () => {
+test('given: valid task parameters; when: creating a task;then: a task with the correct parameters is created;', () => {
     //when:
-    const task = new Task({id,description,sidenote,startDate, endDate: null,deadline, status:true, priority,userId:1})
+    const task = new Task({
+        id,
+        description,
+        sidenote,
+        startDate,
+        endDate: null,
+        deadline,
+        done: false,
+        priority,
+        userId: 1,
+    });
     //then:
     expect(task.getId()).toEqual(id);
     expect(task.getDescription()).toEqual(description);
@@ -22,25 +32,45 @@ test("given: valid task parameters; when: creating a task;then: a task with the 
     expect(task.getStartDate().toLocaleDateString()).toEqual(startDate.toLocaleDateString());
     expect(task.getEndDate()).toEqual(null);
     expect(task.getDeadline().toLocaleDateString()).toEqual(deadline.toLocaleDateString());
-    expect(task.getStatus()).toBeTruthy();
+    expect(task.getDone()).toBeFalsy();
     expect(task.getPriority()).toEqual(priority);
     expect(task.getUserId()).toEqual(1);
 });
 
-test("given: no description; when: creating a task; then: an error is thrown ", () => {
+test('given: no description; when: creating a task; then: an error is thrown ', () => {
     //when:
     const task = () => {
-        new Task({id,description:"",sidenote,startDate, endDate: null,deadline, status:true, priority,userId:1})
+        new Task({
+            id,
+            description: '',
+            sidenote,
+            startDate,
+            endDate: null,
+            deadline,
+            done: false,
+            priority,
+            userId: 1,
+        });
     };
     //then
-    expect(task).toThrow("Description is required.");
+    expect(task).toThrow('Description is required.');
 });
 
-test("given: no userId; when: creating a task; then: an error is thrown ", () => {
+test('given: no userId; when: creating a task; then: an error is thrown ', () => {
     //when:
     const task = () => {
-        new Task({id,description,sidenote,startDate, endDate: null,deadline, status:true, priority,userId:0})
+        new Task({
+            id,
+            description,
+            sidenote,
+            startDate,
+            endDate: null,
+            deadline,
+            done: false,
+            priority,
+            userId: 0,
+        });
     };
     //then
-    expect(task).toThrow("UserId is required.");
+    expect(task).toThrow('UserId is required.');
 });
--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -1,23 +1,20 @@
-import { Priority } from "../model/priority";
+import { Priority } from '../model/priority';
 
 type TaskInput = {
     id?: number;
     description: string;
     sidenote?: string;
     deadline: Date;
-    startDate?:Date;
+    startDate?: Date;
     endDate?: Date;
     priority: PriorityInput;
-    status?: boolean;
+    done?: boolean;
     userId: number;
-}
+};
 
 type PriorityInput = {
     levelName: string;
     colour: string;
-}
+};
 
-export {
-    TaskInput,
-    PriorityInput
-}
+export { TaskInput, PriorityInput };
