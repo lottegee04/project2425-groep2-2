6dcc3c6310c08769d14b6d3caff01847f157912c
lottegee04 <lotte.geeraerts@student.ucll.be>
2024-12-07 20:13:27 +0100

    priority fixed

--- back-end/test/model/priority.test.ts
+++ back-end/test/model/priority.test.ts
@@ -1,12 +1,43 @@
 import { Priority } from "../../model/priority"
+import { Level } from "../../types"
 
 test("given valid priority parameters; when: creating a new priority; then: a priority is created with the right parameters;", () => {
     //given:
     const levelName = "basic"
     const colour = "green"
     //when:
-    const basic = new Priority({levelName,colour})
+    const basic = new Priority({levelName: levelName ,colour: colour});
     //then:
     expect(basic.getLevelName()).toEqual(levelName);
     expect(basic.getColour()).toEqual(colour);
+})
+
+test("given mismatch between priority level basic and colour; when: creating a new priority; then: an error is thrown;", () => {
+        //given:
+        const levelName = "basic"
+        const colour = "red"
+        //when:
+        const basic = ()=>{new Priority({levelName: levelName ,colour: colour});}
+        //then:
+        expect(basic).toThrowError("Basic priority must be green");
+})
+
+test("given mismatch between priority level neutral and colour; when: creating a new priority; then: an error is thrown;", () => {
+    //given:
+    const levelName = "neutral"
+    const colour = "red"
+    //when:
+    const neutral = ()=>{new Priority({levelName: levelName ,colour: colour});}
+    //then:
+    expect(neutral).toThrowError("Neutral priority must be yellow");
+})
+
+test("given mismatch between priority level urgent and colour; when: creating a new priority; then: an error is thrown;", () => {
+    //given:
+    const levelName = "urgent"
+    const colour = "green"
+    //when:
+    const urgent = ()=>{new Priority({levelName: levelName ,colour: colour});}
+    //then:
+    expect(urgent).toThrowError("Urgent priority must be red");
 })
