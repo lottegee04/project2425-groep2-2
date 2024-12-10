d74f18119eba1f3315b78278a5f3a626e6719eb2
OdeMalfaitUCLL <ode.malfait@student.ucll.be>
2024-12-10 11:31:00 +0100

    commented code to click on users to see tasks

--- front-end/components/users/UserOverview.tsx
+++ front-end/components/users/UserOverview.tsx
@@ -1,36 +1,35 @@
 import { User } from "../../types";
-import React from "react"
+import React from "react";
 
 type Props = {
-    users:Array<User>
-    selectUser: (user: User) => void;
-}
+  users: Array<User>;
+  // selectUser: (user: User) => void;
+};
 
-const UserOverview: React.FC<Props> = ({users,selectUser}) =>{
-    return (
-        <>
-        {Array.isArray(users) && 
-        users.map((user, index)=>(
-            <table className="border-4 m-2 bg-secondary bg-opacity-25">
-            <tr key = {index}
-            onClick={() => selectUser(user)}
-            role="button">
-                <td>user id: </td>
-                <td>{user.id}</td>
+const UserOverview: React.FC<Props> = ({ users }) => {
+  return (
+    <>
+      {Array.isArray(users) &&
+        users.map((user, index) => (
+          <table className="border-4 m-2 bg-secondary bg-opacity-25">
+            <tr
+              key={index}
+              // onClick={() => selectUser(user)}
+              role="button"
+            >
+              <td>user id: </td>
+              <td>{user.id}</td>
             </tr>
             <tr>
-                <td>username:</td>
-                <td>{user.username}</td>
+              <td>username:</td>
+              <td>{user.username}</td>
             </tr>
-            <tr>
-            </tr>
-            </table>
-        ))
-        }
-        {!users&&
-        <p>No Active Users</p>}
-        </>
-    )
-}
+            <tr></tr>
+          </table>
+        ))}
+      {!users && <p>No Active Users</p>}
+    </>
+  );
+};
 
 export default UserOverview;
--- front-end/pages/users/index.tsx
+++ front-end/pages/users/index.tsx
@@ -1,62 +1,79 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { Task, User } from '../../types';
-import TaskService from '../../services/TaskService';
-import Header from '../../components/header';
-import TaskOverview from '../../components/tasks/TaskOverview';
-import UserService from '../../services/UserService';
-import UserOverview from '../../components/users/UserOverview';
-import TasksByUser from '../../components/tasks/TasksByUser';
-import useSWR, { mutate } from 'swr';
-import useInterval from 'use-interval';
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Task, User } from "../../types";
+import TaskService from "../../services/TaskService";
+import Header from "../../components/header";
+import TaskOverview from "../../components/tasks/TaskOverview";
+import UserService from "../../services/UserService";
+import UserOverview from "../../components/users/UserOverview";
+import TasksByUser from "../../components/tasks/TasksByUser";
+import useSWR, { mutate } from "swr";
+import useInterval from "use-interval";
 
 const Users: React.FC = () => {
-    const [selectedUser,setSelectedUser] = useState<User>();
-    const [error, seterror] = useState<string | null>(null);
-    const [loggedInUser, setLoggedInUser] = useState<User>(null);
-    useEffect(() => {setLoggedInUser(JSON.parse(localStorage.getItem("loggedInUser")));
-    }, []);
-    const getUsers = async () =>{
-        seterror(null);
-        const response = await UserService.getUsers()
-        if (response.ok) {
-        const users = await response.json();
-        return {users}
-      } else {
-        seterror("You are not authorized to view this page. Log in to see this content!")
-        return {users: []};
-      }
+  // const [selectedUser, setSelectedUser] = useState<User>();
+  const [error, seterror] = useState<string | null>(null);
+  const [loggedInUser, setLoggedInUser] = useState<User>(null);
+  useEffect(() => {
+    setLoggedInUser(JSON.parse(localStorage.getItem("loggedInUser")));
+  }, []);
+  const getUsers = async () => {
+    seterror(null);
+    const response = await UserService.getUsers();
+    if (response.ok) {
+      const users = await response.json();
+      return { users };
+    } else {
+      seterror(
+        "You are not authorized to view this page. Log in to see this content!"
+      );
+      return { users: [] };
+    }
   };
-  const {data, isLoading} = useSWR("users",getUsers);
+  const { data, isLoading } = useSWR("users", getUsers);
   useInterval(() => {
-    if (!error) mutate("users",getUsers());
-  }, 2000);
+    if (!error) mutate("users", getUsers());
+  }, 5000);
 
   return (
     <>
-    <Head>
+      <Head>
         <title>Users</title>
-    </Head>
-    <Header/>
-    <main className='d-flex  flex-column '>
-    <h1 className='align-self-center'>{" "}
-    {loggedInUser && `${loggedInUser.role === "admin"? "All Users (admin)": `User: ${loggedInUser.username}`}`}</h1>
-    {!error && <p className='align-self-center'>Click on the user to see their tasks.</p>}
-    <section className='align-self-center d-flex flex-row p-2 '>
-    {error && <p className="text-[#b62626]">{error}</p>}
-    {!error &&isLoading && <p className="text-[#2866da]">Loading....</p>}
-    {!error && data && 
-      <UserOverview users={data.users} selectUser={setSelectedUser}></UserOverview>
-    }
-    </section>
-    {!error && selectedUser && (
+      </Head>
+      <Header />
+      <main className="d-flex  flex-column ">
+        <h1 className="align-self-center">
+          {" "}
+          {loggedInUser &&
+            `${
+              loggedInUser.role === "admin"
+                ? "All Users (admin)"
+                : `User: ${loggedInUser.username}`
+            }`}
+        </h1>
+        {!error && (
+          <p className="align-self-center">
+            Click on the user to see their tasks.
+          </p>
+        )}
+        <section className="align-self-center d-flex flex-row p-2 ">
+          {error && <p className="text-[#b62626]">{error}</p>}
+          {!error && isLoading && <p className="text-[#2866da]">Loading....</p>}
+          {!error && data && (
+            <UserOverview
+              users={data.users}
+              // selectUser={setSelectedUser}
+            ></UserOverview>
+          )}
+        </section>
+        {/* {!error && selectedUser && (
          <section className='align-self-center d-flex flex-column p-2 '>
          <h2> Tasks from "{selectedUser.username}"</h2>
          <TasksByUser user={selectedUser}></TasksByUser>
-       </section>
-    )}
-    </main>
+       </section> */}
+        {/* )} */}
+      </main>
     </>
   );
 };
